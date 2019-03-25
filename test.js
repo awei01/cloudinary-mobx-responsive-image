7d557f214b1f64bcb1a13d5a535119cff88bd699
Austin Wei <awei@Austins-MacBook-Pro.local>
2019-03-25 13:00:00 -0400

    moving tests around and faking the window.Image for tests

--- test.js
+++
@@ -1,6 +0,0 @@
-import test from 'ava'
-import extractCloudinaryDataSpec from './tests/extractCloudinaryData.spec'
-import imageSpec from './tests/Image.spec'
-
-extractCloudinaryDataSpec(test)
-imageSpec(test)
--- tests/fixtures.js
+++
@@ -1,4 +0,0 @@
-export const images = {
-  simple: 'http://res.cloudinary.com/demo/image/upload/folder/sample.png',
-  versioned: 'http://res.cloudinary.com/demo/image/upload/v1429686295/folder/sample.png'
-}
